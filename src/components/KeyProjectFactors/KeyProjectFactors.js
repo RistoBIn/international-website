03081996aad03501e5d9b888973bc1e07fad8734
Fredrik Sigvartsen <fredrik@sealab.no>
2020-03-27 14:49:11 +0100

    Remove unecassary css, and edit according to design

--- src/components/KeyProjectFactors/KeyProjectFactors.js
+++ src/components/KeyProjectFactors/KeyProjectFactors.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import classNames from 'classnames';
 import styles from './KeyProjectFactors.module.scss';
 
 const KeyProjectFactors = ({ keyFactors }) => {
   return (
     <>
       <div id="key-factors" className={styles.wrapper}>
-        <h5 className={styles.heading} >{keyFactors.heading}</h5>
+        <h3 className={styles.heading}>{keyFactors.heading}</h3>
         {keyFactors.factorItems.map(factor => (
           <div className={styles.factorItem}>
             <p className={styles.description}> {factor.description} </p>
