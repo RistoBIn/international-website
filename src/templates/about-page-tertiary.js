b59b6cf502009822325c9794a8df74e3432065ce
thomasur <thomas@sealab.no>
2020-03-30 18:13:51 +0200

    Fix encoding issue

--- src/templates/about-page-tertiary.js
+++ src/templates/about-page-tertiary.js
@@ -1,34 +1,39 @@
 import React from 'react';
-​
+
 import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import Content, { HTMLContent } from '../components/Content';
-​
+
 export const AboutPageTemplate = ({
   content,
   contentComponent,
-  description,
   heading,
+  description,
 }) => {
   const PostContent = contentComponent || Content;
-​
+
   return (
-    <section className="section has-dark-background">
-      <div className="container">
-        <h1>{heading}</h1>
-        <p>{description}</p>
-        <p>This is product-page-secondary</p>
-        <PostContent content={content} />
-      </div>
-    </section>
+    <>
+      <section className="section has-dark-background about-page-primary">
+        <div className="container">
+          <h1 className="section--title">{heading}</h1>
+          <p className="section--description">{description}</p>
+        </div>
+      </section>
+      <section className="section has-dark-background about-page-primary">
+        <div className="container">
+          <PostContent content={content} className="content is-left-aligned" />
+        </div>
+      </section>
+    </>
   );
 };
-​
+
 const AboutPage = ({ data }) => {
   const { frontmatter } = data.markdownRemark;
   if (!frontmatter) return <></>;
-  const { title, seoDescription, heading, description } = frontmatter;
-​
+  const { title, heading, description, seoDescription } = frontmatter;
+
   return (
     <Layout seoTitle={title} seoDescription={seoDescription}>
       <AboutPageTemplate
@@ -40,19 +45,17 @@ const AboutPage = ({ data }) => {
     </Layout>
   );
 };
-​
+
 export default AboutPage;
-​
+
 export const pageQuery = graphql`
-  query AboutPageTertiaryById($id: String!) {
+  query aboutPageTertiaryTemplateById($id: String!) {
     markdownRemark(id: { eq: $id }) {
       id
       html
       frontmatter {
         title
-        seoDescription
         description
-        heading
       }
     }
   }
