d68e0a32d4dafc3ea0be196ef63b4d498dda9b8d
thomasur <thomas@sealab.no>
2020-03-31 14:03:24 +0200

    Add template and markdown for Partners page. (WIP)

---
+++ src/pages/about/partners.md
@@ -0,0 +1,31 @@
+---
+templateKey: about-page-partners
+title: Partners
+seoDescription: SEALAB Partners.
+heading: Partners
+description: >-
+    SEALAB partners
+    some more
+partners:
+  - icon: /img/TELENOR-WHITE.png
+    url: https://www.telenor.no
+  - icon: /img/microsoftAzure.png
+    url: https://www.azure.microsoft.com
+  - icon: /img/intel-logo-white.png
+    url: https://www.intel.com
+  - icon: /img/monitorfish-logo.svg
+    url: https://www.monitorfish.com
+  - icon: /img/innovation-norway-logo.svg
+    url: https://www.innovasjonnorge.no/en/start-page/
+  - icon: /img/flag-of-europe.svg
+    url: https://ec.europa.eu/programmes/horizon2020/en
+  - icon: /img/reseach-council-logo.svg
+    url: https://www.forskningsradet.no/en/
+  - icon: /img/sealab-logo-white.svg
+    url: https://www.sintef.no/en/ocean/
+  - icon: /img/ntnu-logo-white.svg
+    url: https://www.ntnu.edu/
+  - icon: /img/mausund-feltstasjon-logo.png
+    url: https://www.eider.no/
+---
+
---
+++ src/templates/about-page-partners.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import Content, { HTMLContent } from '../components/Content';
+
+export const AboutPagePartnersTemplate = ({
+  content,
+  contentComponent,
+  heading,
+  description,
+}) => {
+  const PostContent = contentComponent || Content;
+
+  return (
+    <>
+      <section className="section has-dark-background about-page-primary">
+        <div className="container">
+          <h1 className="section--title">{heading}</h1>
+          <p className="section--description">{description}</p>
+        </div>
+      </section>
+      <section className="section has-dark-background about-page-primary">
+        <div className="container">
+          <PostContent content={content} className="content is-left-aligned" />
+        </div>
+      </section>
+    </>
+  );
+};
+
+const AboutPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark;
+  if (!frontmatter) return <></>;
+  const { title, heading, description, seoDescription } = frontmatter;
+
+  return (
+    <Layout seoTitle={title} seoDescription={seoDescription}>
+      <AboutPagePartnersTemplate
+        content={data.markdownRemark.html}
+        contentComponent={HTMLContent}
+        heading={heading}
+        description={description}
+      />
+    </Layout>
+  );
+};
+
+export default AboutPage;
+
+export const pageQuery = graphql`
+  query aboutPagePartnersTemplateById($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        title
+        seoDescription
+        description
+        heading
+        partners {
+          icon {
+            publicURL
+            extension
+            childImageSharp {
+              fluid(maxWidth: 90, quality: 80) {
+                ...GatsbyImageSharpFluid_noBase64
+                presentationWidth
+              }
+            }
+          }
+          url
+        }
+      }
+    }
+  }
+`;
