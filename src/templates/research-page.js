5567f3b6273fd29cde3c1c07c7cc47a7f0cbb9d0
Fredrik Sigvartsen <fredrik@sealab.no>
2020-04-08 14:52:26 +0200

    Split section

--- src/templates/research-page.js
+++ src/templates/research-page.js
@@ -51,13 +51,72 @@ const ImageGrid = styled.div`
   }
 `;
 
+const ImageGridStairs = styled.div`
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: 2fr 1fr 1fr;
+  grid-template-rows: 1fr;
+  padding-top: 60px;
+  .image-item .image {
+    min-height: 440px;
+  }
+  @media only screen and (max-width: 960px) {
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: 1fr 1fr;
+    .image-item:first-of-type {
+      grid-column: 1 / span 2;
+      grid-row: 1;
+    }
+    .image-item:nth-of-type(2) {
+      grid-column: 1;
+      grid-row: 2;
+    }
+    .image-item:nth-of-type(3) {
+      grid-column: 2;
+      grid-row: 2;
+    }
+    .image-item {
+      .image {
+        height: 100% !important;
+        width: 100% !important;
+      }
+    }
+  }
+  @media only screen and (min-width: 960px) {
+    .image-item:nth-of-type(2) {
+      margin-top: 60px;
+    }
+    .image-item:nth-of-type(3) {
+      margin-top: 120px;
+    }
+  }
+`;
+
+const CareersList = styled.ul`
+  li {
+    padding: 16px 0;
+    font-size: 22px;
+    font-weight: 600 !important;
+    color: white !important;
+    line-height: 350%;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.15);
+  }
+`;
+
+const FreeText = styled.section`
+  p strong {
+    color: #dbe0ea !important;
+    font-size: 32px !important;
+  }
+`;
+
 const ButtonFlex = styled.div`
   .button {
     margin-top: 20px;
   }
   @media only screen and (min-width: 768px) {
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(160px, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(160px, 200px));
     grid-gap: 12px;
   }
 `;
@@ -69,6 +128,8 @@ export const ResearchPageTemplate = ({
   description,
   featuredimages,
   centeredSection,
+  careers,
+  splitSection,
 }) => {
   const PostContent = contentComponent || Content;
 
@@ -108,7 +169,78 @@ export const ResearchPageTemplate = ({
             path={centeredSection.button.path}
           />
         </div>
+        <ImageGridStairs>
+          {centeredSection.images.map(imageItem => (
+            <div className="image-item">
+              <NonStretchedImage
+                objectFit="contain"
+                alt=""
+                className="image"
+                {...imageItem.image}
+              />
+            </div>
+          ))}
+        </ImageGridStairs>
       </section>
+      <SplittedSection
+        leftColumn={
+          <>
+            <p className="subheading">{careers.subheading}</p>
+            <h2>{careers.heading}</h2>
+            <PostContent
+              content={generateHTML(careers.left)}
+              className="content "
+            />
+            <ButtonFlex>
+              {careers.buttons.map(buttonObject => (
+                <Button
+                  className="is-transparent"
+                  text={buttonObject.text}
+                  path={buttonObject.path}
+                />
+              ))}
+            </ButtonFlex>
+          </>
+        }
+        rightColumn={
+          <>
+            <p>
+              <strong>{careers.positions.heading}</strong>
+            </p>
+            <CareersList>
+              {careers.positions.items.map(positionText => (
+                <li>{positionText}</li>
+              ))}
+            </CareersList>
+          </>
+        }
+        className="section has-dark-background"
+      />
+      <FreeText className="section has-dark-background">
+        <div className="container content centered-free-text">
+          <PostContent content={content} className="content " />
+        </div>
+      </FreeText>
+      <SplittedSection
+        leftColumn={
+          <PostContent
+            content={generateHTML(splitSection.left)}
+            className="content "
+          />
+        }
+        leftColumnCSS=""
+        rightColumn={
+          <NonStretchedImage
+            objectFit="contain"
+            fluid={splitSection.featuredimage.childImageSharp.fluid}
+            alt=""
+            className="image"
+          />
+        }
+        rightColumnCSS="centered"
+        className="section has-dark-background"
+        shouldReorderOnMobile
+      />
     </>
   );
 };
@@ -201,7 +333,7 @@ export const pageQuery = graphql`
           left
           featuredimage {
             childImageSharp {
-              fluid(maxWidth: 600, quality: 80) {
+              fluid(maxWidth: 650, quality: 80) {
                 ...GatsbyImageSharpFluid_noBase64
                 presentationWidth
               }
