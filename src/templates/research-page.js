6ee8e780378065879c64be655c9edc7c65dcd695
Fredrik Sigvartsen <fredrik@sealab.no>
2020-04-08 15:11:09 +0200

    Remove unused

--- src/templates/research-page.js
+++ src/templates/research-page.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { graphql } from 'gatsby';
-import Img from 'gatsby-image';
 import styled from 'styled-components';
 import BackgroundImage from '../components/BackgroundImage';
 import Layout from '../components/Layout';
