a2fdabfe9e6d60a933c7833dd98337196efeb15d
Fredrik Sigvartsen <fredrik@sealab.no>
2020-04-30 10:33:21 +0200

    Creating better sitemaps

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     email: 'contact@sealab.no',
     phone: '+47 729 09 111',
     siteUrl: `https://www.sealab.no/`,
-    },
+  },
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
@@ -38,7 +38,6 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
-          
           {
             resolve: 'gatsby-remark-relative-images',
           },
@@ -65,6 +64,44 @@ module.exports = {
     'gatsby-plugin-netlify-cache',
     'gatsby-plugin-robots-txt',
     'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        exclude: [`/404/*`, `/admin/*`, '/privacy-policy/*'],
+        serialize: ({ site, allSitePage }) =>
+          allSitePage.edges.map(edge => {
+            let path = edge.node.path;
+            let priority = 0.6;
+            if (path.match(/products/)) {
+              priority = 1.0;
+            } else if (path.match(/about/)) {
+              priority = 0.6;
+            } else if (path.match(/contact/)) {
+              priority = 1.0;
+            } else if (path.match(/history/)) {
+              priority = 0.8;
+            } else if (path.match(/press/)) {
+              priority = 0.8;
+            } else if (path.match(/investors/)) {
+              priority = 1.0;
+            } else if (path.match(/leadership/)) {
+              priority = 0.8;
+            } else if (path.match(/research-laboratories/)) {
+              priority = 0.8;
+            } else if (path.match(/about\/partnership/)) {
+              priority = 1.0;
+            } else if (path.match(/sealab-tv-channel/)) {
+              priority = 0.8;
+            }
+
+            return {
+              url: site.siteMetadata.siteUrl + edge.node.path,
+              changefreq: `daily`,
+              priority,
+            };
+          }),
+      },
+    },
     'gatsby-plugin-catch-links',
     {
       resolve: 'gatsby-plugin-netlify-cms',
@@ -73,13 +110,12 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-react-svg",
+      resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
-          include: /\.inline\.svg$/ // See below to configure properly
-        }
-      }
-   
+          include: /\.inline\.svg$/, // See below to configure properly
+        },
+      },
     },
     {
       resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
@@ -99,16 +135,16 @@ module.exports = {
     //   resolve: "gatsby-plugin-google-tagmanager",
     //   options: {
     //     id: process.env.GOOGLE_TAG_MANAGER_ID,
-  
+
     //     // Include GTM in development.
     //     // Defaults to false meaning GTM will only be loaded in production.
     //     includeInDevelopment: false,
-  
+
     //     // datalayer to be set before GTM is loaded
     //     // should be an object or a function that is executed in the browser
     //     // Defaults to null
     //     defaultDataLayer: { platform: "gatsby" },
-  
+
     //     // Specify optional GTM environment details.
     //     // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
     //     // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
@@ -117,4 +153,4 @@ module.exports = {
     // },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
-}
+};
