7b16be645374c4db10488f2043cee9c4c0032590
Fredrik Sigvartsen <fredrik@sealab.no>
2020-05-14 08:38:38 +0200

    Update remark-pages

--- src/templates/about-page-partners.js
+++ src/templates/about-page-partners.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { graphql } from 'gatsby';
-import Img from 'gatsby-image';
 import Layout from '../components/Layout';
 import NonStretchedImage from '../components/NonStretchedImage';
 import Content, { HTMLContent } from '../components/Content';
