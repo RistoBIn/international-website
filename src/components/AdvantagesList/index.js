b50cf9938d60341c6e67cdf2857ee5fe85612479
RistoBin <ristobinovski@gmail.com>
2020-06-08 13:27:42 +0200

    update styles order

--- src/components/AdvantagesList/index.js
+++ src/components/AdvantagesList/index.js
@@ -3,25 +3,29 @@ import styled from 'styled-components';
 import Img from 'gatsby-image'
 
 const Wrap = styled.div`
-	margin-top: 42px;
 	display: grid;
 	grid-template-columns: repeat(5, 1fr);
+
+	margin-top: 42px;
+	
 	.gatsby-image-wrapper {
-		max-width: 92px;
 		width: 100%;
+		max-width: 92px;
 		margin: auto;
 	}
 	p {
-		text-align: center;
 		margin-top: 55px;
+
 		font-style: normal;
 		font-weight: bold;
 		font-size: 14px;
 		line-height: 140%;
+		text-align: center;
 	}
 	@media(max-width: 768px) {
 		grid-template-columns: repeat(1, 1fr);
 		grid-row-gap: 20px;
+		
 		p {
 			max-width: 191px;
 			margin: 55px auto 0;
@@ -30,7 +34,9 @@ const Wrap = styled.div`
 `
 const AdvantageItem = styled.div`
   padding: 10px 30px;
-  border-left: ${props=>props.bl};
+
+  border-left: ${props=>props.borderLeft};
+
   @media(max-width: 768px) {
   	border-left: 0px;
   }
@@ -41,7 +47,7 @@ const AdvantagesList = ({advantages}) => (
 		{
 			advantages.map((item, i)=>{
 				return(
-					<AdvantageItem key={i} bl={i===0?"none":"1px solid rgba(255, 255, 255, 0.2)"} >
+					<AdvantageItem key={i} borderLeft={i===0?"none":"1px solid rgba(255, 255, 255, 0.2)"} >
 						<Img fluid={item.featuredimage.childImageSharp.fluid} />
 						<p>{item.advantage}</p>
 					</AdvantageItem>
--- src/components/CheckList/index.js
+++ src/components/CheckList/index.js
@@ -3,30 +3,35 @@ import styled from 'styled-components';
 import CheckMark from '../../img/check-icon.svg';
 
 const Wrapper = styled.div`
-	margin-top: 66px;
 	display: grid;
 	grid-template-columns: repeat(2, 1fr);
 	grid-template-rows: auto;
 	grid-column-gap: 80px;
 	grid-row-gap: 25px;
+	margin-top: 66px;
+	
 	@media(max-width: 600px) {
 		margin-top: 55px;
 		grid-template-columns: repeat(1, 1fr);
 	}
 `
 const CheckItem = styled.div`
-	background: linear-gradient(270deg, rgba(69, 183, 210, 0) 0%, #226CC7 100%);
-	border-radius: 40px;
 	display: flex;
 	align-items: center;
-	font-weight: 600;
+
+	background: linear-gradient(270deg, rgba(69, 183, 210, 0) 0%, #226CC7 100%);
+	border-radius: 40px;
+		
 	font-size: 16px;
+	font-weight: 600;
 	line-height: 100%;
+	
 	& img {
-		padding: 15px 13px;
 		margin-right: 10px;
-		border-radius: 20px;
+		padding: 15px 13px;
+		
 		background: rgba(255, 255, 255, 0.1);
+		border-radius: 20px;
 	}
 `
 const CheckList = ({features}) => {
